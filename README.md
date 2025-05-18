81a4cd8c93a0b5e4ff449645b40f8c11b93acacc
CrazyCat911 <127078477+CrazyCat911@users.noreply.github.com>
2025-05-18 20:13:52 +1000

    Make everything i guess

---
+++ README.md
@@ -0,0 +1,6 @@
+# crazycat911.github.io
+
+My site
+
+---
+(It's pretty useless)
---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	let chars =
+		'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-_=+`~,.<>/\\;:\'"';
+
+	async function randomise(e: Event) {
+		let target = e.target as HTMLElement;
+		let oldtext = target.dataset.oldtext;
+		for (let i = 0; i < 25; i++) {
+			await new Promise((r) => {
+				setTimeout(r, 25);
+			});
+			target.innerText = chars[Math.floor(Math.random() * chars.length)];
+		}
+		target.innerText = oldtext || '!';
+	}
+</script>
+
+<h1>
+	{#each 'Crazy_Cat911' as letter}
+		<span role="none" onmouseenter={randomise} data-oldtext={letter}>{letter}</span>
+	{/each}
+</h1>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Syne+Mono&display=swap');
+
+	h1 {
+		font-family: 'Syne Mono', monospace;
+		font-size: 250px;
+	}
+</style>
